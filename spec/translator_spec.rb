0a29692de6ed53f1eb8667faa9b0a535a96f056d
Rafael George <george.rafael@gmail.com>
2009-02-03 10:27:27 -0400

    Modification of code structure
    - Added new constant for all languages

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -9,23 +9,23 @@ describe Translator do
   end
 
   it "should return 'buenas tardes'" do
-    @babel.translate(Translator::Constants::ENGLISH, Translator::Constants::SPANISH).should == "buenas tardes"
+    @babel.translate(Translator::Constants::LANGUAGES[:english], Translator::Constants::LANGUAGES[:spanish]).should == "buenas tardes"
   end
 
   it "should return 'bonjour'" do
-    @babel.translate(Translator::Constants::ENGLISH, Translator::Constants::FRENCH).should == "bonjour"
+    @babel.translate(Translator::Constants::LANGUAGES[:english], Translator::Constants::LANGUAGES[:french]).should == "bonjour"
   end
 
   it "should return 'Guten Tag'" do
-    @babel.translate(Translator::Constants::ENGLISH, Translator::Constants::GERMAN).should == "Guten Tag"
+    @babel.translate(Translator::Constants::LANGUAGES[:english], Translator::Constants::LANGUAGES[:german]).should == "Guten Tag"
   end
 
   it "should return 'buon pomeriggio'" do
-    @babel.translate(Translator::Constants::ENGLISH, Translator::Constants::ITALIAN).should == "buon pomeriggio"
+    @babel.translate(Translator::Constants::LANGUAGES[:english], Translator::Constants::LANGUAGES[:italian]).should == "buon pomeriggio"
   end
 
   it "should return 'boa tarde'" do
-    @babel.translate(Translator::Constants::ENGLISH, Translator::Constants::PORTUGESE).should == "boa tarde"
+    @babel.translate(Translator::Constants::LANGUAGES[:english], Translator::Constants::LANGUAGES[:portugese]).should == "boa tarde"
   end
 
   it "should translate to japanese" do
